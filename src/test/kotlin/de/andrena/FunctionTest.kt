c919be6c72dec5935436f591cec25abbd5e2645b
Patrick Neulichedl <patrick.neulichedl@andrena.de>
2021-05-02 09:24:31 +0200

    Enhance tests.

--- src/test/kotlin/de/andrena/FunctionTest.kt
+++ src/test/kotlin/de/andrena/FunctionTest.kt
@@ -1,5 +1,6 @@
 package de.andrena
 
+import com.microsoft.azure.functions.HttpStatus.BAD_REQUEST
 import com.microsoft.azure.functions.HttpStatus.OK
 import de.andrena.util.mockContext
 import de.andrena.util.preconfiguredGetRequest
@@ -10,7 +11,16 @@ import org.junit.jupiter.api.Test
 class FunctionTest {
 
     @Test
-    fun `get with queryParameter name returns OK`() {
+    fun `get without queryParameter name returns BadRequest`() {
+        val request = preconfiguredGetRequest(queryParameters = emptyMap())
+
+        val response = Function().get(request, mockContext())
+
+        response.status shouldBe BAD_REQUEST
+    }
+
+    @Test
+    fun `get with queryParameter name returns Ok`() {
         val request = preconfiguredGetRequest(queryParameters = mapOf("name" to "Azure"))
 
         val response = Function().get(request, mockContext())
@@ -19,7 +29,16 @@ class FunctionTest {
     }
 
     @Test
-    fun `post with name in body returns OK`() {
+    fun `post without name in body returns BadRequest`() {
+        val request = preconfiguredPostRequest<String?>(body = null)
+
+        val response = Function().post(request, mockContext())
+
+        response.status shouldBe BAD_REQUEST
+    }
+
+    @Test
+    fun `post with name in body returns Ok`() {
         val request = preconfiguredPostRequest<String?>(body = "Azure")
 
         val response = Function().post(request, mockContext())
